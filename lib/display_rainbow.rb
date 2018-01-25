7256bcd7468da6b077cf2eb5d7622e254f69afb9
Angelo Cigna <acigna1982@gmail.com>
2018-01-25 00:03:46 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@ array_of_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet
 letters = ["R:", "O:", "Y:", "G:", "B:", "I:", "V:"]
 def display_rainbow(array_of_colors)
 
-puts "#{letters[0]}#{array_of_colors[0]}"
+puts "#{letters[0]}#{array_of_colors[0]}, #{letters[0]}#{array_of_colors[0]},#{letters[0]}#{array_of_colors[0]},#{letters[0]}#{array_of_colors[0]},#{letters[0]}#{array_of_colors[0]},#{letters[0]}#{array_of_colors[0]},"
 
 
 end #end def
